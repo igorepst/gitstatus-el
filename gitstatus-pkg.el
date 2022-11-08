6c5024ad76fa9bbf2e3bea8d03e77e1767fd3aee
Igor Epstein <igor.epstein@moovit.com>
2022-11-08 10:01:04 +0200

    Cont. README

---
+++ gitstatus-pkg.el
@@ -0,0 +1,2 @@
+;; -*- no-byte-compile: t -*-
+(define-package "gitstatus" "0.1.0" "Front-end for gitstatusd" '((emacs "25.1")) :authors '(("Igor Epstein" . "igorepst@gmail.com")) :maintainer '("Igor Epstein" . "igorepst@gmail.com") :url "https://github.com/igorepst/gitstatus-el")
